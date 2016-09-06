568902a268587dfe8fa0010fd900d45b5e990db2
Eric Fournier <eric.fournier@fsaa.ulaval.ca>
2016-09-06 10:09:22 -0400

    Do intergenic counts.

--- scripts/DefineIntergenicRegions.sh
+++ scripts/DefineIntergenicRegions.sh
@@ -25,6 +25,15 @@ bedtools subtract -a input/All.bed -b output/intergenic/UCSC_SGD_Genes.nochr.bed
 # Split and annotate all intergenic regions based on their proximity to the closest gene.
 perl scripts/annotate_split.pl output/intergenic/UCSC_SGD_Genes.nochr.bed 1000 < output/intergenic/intergenic.bed > output/intergenic/intergenic.split.bed
 
+
+for type in promoter downstream
+do
+    grep -e "$type.*+$" output/intergenic/intergenic.split.bed > $type.plus.bed
+    grep -e "$type.*-$" output/intergenic/intergenic.split.bed > $type.minus.bed
+done
+
+grep -e "intergenic" output/intergenic/intergenic.split.bed > intergenic.bed
+
 # Split the bam files
 for lib in CK2 WT Spt6-SA
 do
@@ -33,9 +42,6 @@ do
      
     for type in promoter downstream
     do
-        grep -e "$type.*+$" output/intergenic/intergenic.split.bed > plus.bed
-        grep -e "$type.*-$" output/intergenic/intergenic.split.bed > minus.bed
-        
         for direction in sense antisense
         do
             plus_samtools_arg="-f"
@@ -48,52 +54,56 @@ do
             
             prefix=output/intergenic/$lib.$type.$direction
             
-            samtools view -b -h $plus_samtools_arg 16 -L plus.bed $BAMFILE > $prefix.1.bam
-            samtools view -b -h $minus_samtools_arg 16 -L minus.bed $BAMFILE > $prefix.2.bam
-            samtools merge $prefix.bam $prefix.1.bam $prefix.2.bam
-            samtools sort -o $prefix.sorted.bam $prefix.bam
-            samtools index $prefix.sorted.bam
-            rm $prefix.1.bam $prefix.2.bam $prefix.bam        
+            cat $lib.$type.$direction.generate_bam.sh <<EOF
+samtools view -b -h $plus_samtools_arg 16 -L $type.plus.bed $BAMFILE > $prefix.1.bam
+samtools view -b -h $minus_samtools_arg 16 -L $type.minus.bed $BAMFILE > $prefix.2.bam
+samtools merge $prefix.bam $prefix.1.bam $prefix.2.bam
+samtools sort -o $prefix.sorted.bam $prefix.bam
+samtools index $prefix.sorted.bam
+rm $prefix.1.bam $prefix.2.bam $prefix.bam        
+EOF
+
+            
          done
-         
-         rm plus.bed minus.bed
     done
     
-    grep -e "intergenic" output/intergenic/intergenic.split.bed > intergenic.bed
-    samtools view -b -h -L intergenic.bed $BAMFILE > output/intergenic/$lib.intergenic.bam
-    samtools sort -o output/intergenic/$lib.intergenic.sorted.bam output/intergenic/$lib.intergenic.bam
-    samtools index output/intergenic/$lib.intergenic.sorted.bam
+    cat $lib.intergenic.generate_bam.sh <<EOF
+samtools view -b -h -L intergenic.bed $BAMFILE > output/intergenic/$lib.intergenic.bam
+samtools sort -o output/intergenic/$lib.intergenic.sorted.bam output/intergenic/$lib.intergenic.bam
+samtools index output/intergenic/$lib.intergenic.sorted.bam
+EOF
+
 done
 
-module load mugqic_dev/python/2.7.8
+module load mugqic/python/2.7.8
 for lib in CK2 WT Spt6-SA
 do
     for type in promoter downstream
     do
-        grep -e "$type" output/intergenic/intergenic.split.bed > type.bed
+        grep -e "$type" output/intergenic/intergenic.split.bed > $type.bed
     
         for direction in sense antisense
         do
             prefix=output/intergenic/$lib.$type.$direction
             BAMFILE=$prefix.sorted.bam
             BIGWIG=$prefix.sorted.bigwig
-            
-            ./bin/bamCoverage --skipNonCoveredRegions --binSize 10 --numberOfProcessors 8 -b $BAMFILE -o $BIGWIG
-            ./bin/computeMatrix reference-point --nanAfterEnd --numberOfProcessors 8 --regionsFileName type.bed --scoreFileName $BIGWIG --outFileName $prefix.sorted.matrix.gz
+                
+            cat $lib.$type.$direction.generate_matrix.sh <<EOF
+./bin/bamCoverage --skipNonCoveredRegions --binSize 10 --numberOfProcessors 8 -b $BAMFILE -o $BIGWIG
+./bin/computeMatrix reference-point --nanAfterEnd --numberOfProcessors 8 --regionsFileName $type.bed --scoreFileName $BIGWIG --outFileName $prefix.sorted.matrix.gz
+EOF
+
         done
-        
-        rm type.bed
     done
-    
-    grep -e "intergenic" output/intergenic/intergenic.split.bed > intergenic.bed
     
     # And finally, do intergenic.
     BAMFILE=output/intergenic/$lib.intergenic.sorted.bam
     BIGWIG=output/intergenic/$lib.intergenic.sorted.bigwig
     
-    ./bin/bamCoverage --skipNonCoveredRegions --binSize 10 --numberOfProcessors 8 -b $BAMFILE -o $BIGWIG
-    ./bin/computeMatrix scale-regions --numberOfProcessors 8 --regionsFileName intergenic.bed --scoreFileName $BIGWIG --outFileName $prefix.sorted.matrix.gz
-    
-    rm intergenic.bed
+    cat $lib.$type.$direction.generate_matrix.sh <<EOF
+./bin/bamCoverage --skipNonCoveredRegions --binSize 10 --numberOfProcessors 8 -b $BAMFILE -o $BIGWIG
+./bin/computeMatrix scale-regions --numberOfProcessors 8 --regionsFileName intergenic.bed --scoreFileName $BIGWIG --outFileName $prefix.sorted.matrix.gz
+EOF
+
 done
 
